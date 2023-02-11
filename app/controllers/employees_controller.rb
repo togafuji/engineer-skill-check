70812c442948946c0babbf8ec54d7d7b7325c4af
togafuji <110988972+togafuji@users.noreply.github.com>
2023-02-11 09:27:55 +0900

    ストロングパラメーターにお知らせ投稿権限のカラムを追加

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -43,7 +43,7 @@ class EmployeesController < ApplicationController
   private
 
   def employee_params
-    params.require(:employee).permit(:number, :last_name, :first_name, :account, :password, :department_id, :office_id, :email, :date_of_joining, :employee_info_manage_auth)
+    params.require(:employee).permit(:number, :last_name, :first_name, :account, :password, :department_id, :office_id, :email, :date_of_joining, :employee_info_manage_auth, :news_posting_auth)
   end
 
   def set_employee
